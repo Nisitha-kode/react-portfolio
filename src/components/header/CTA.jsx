d6c67aeca3d576fa7d354e00b2058efc6046886d
NisithaPrakash <nisitha.kodehode@gmail.com>
2022-11-24 11:28:27 +0100

    Add some changes

---
+++ src/components/header/CTA.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+//import CV from '../header/cv.pdf'
+const CTA = () => {
+  return (
+    <div className='cta'>
+      <a href='#contact'>Let's Talk</a>
+    </div>
+  )
+}
+
+export default CTA
+//<a href='../../Assets/cv.pdf' download className='btn'>
+//<a href={CV} download>Download CV</a>
